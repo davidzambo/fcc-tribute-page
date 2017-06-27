73b152a09c92b453a49353ea625248a1ea77fec0
Zámbó Sándor Dávd <david.a.zambo@gmail.com>
2017-06-27 08:35:48 +0200

    Tribute page is ready

---
+++ index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
+  <title>Tribute page to Muhammad Ali</title>
+</head>
+
+<style>
+body {
+  padding: 50px 50px 0 50px;
+  background-color: #000000;
+}
+
+h1 {
+  padding-top: 90px;
+  font-size: 4em;
+}
+
+p i {
+  font-size: 1.6em;
+}
+i {
+  padding-bottom: 5px;
+}
+
+.content{
+  width: 100%;
+  background-color: lightGrey;
+  border-radius: 5px;
+}
+
+.img-container{
+   background-color: white;
+   border-radius: 5px;
+   padding: 3px;
+   border: 1px solid #DDD;
+   font-family: sans-serif;
+   font-size: 0.9em;
+}
+
+.img-css {
+  width: 100%;
+  border-radius: 5px;
+  margin-bottom: 15px;
+}
+
+.biography {
+  margin-top: 10px;
+  text-align: left;
+  font-family: sans-serif;
+}
+
+ul {
+  font-size: 0.9em;
+}
+
+li {
+  margin: 0px;
+}
+
+blockquote {
+  padding: 25px;
+}
+
+q {
+  font-size: 1.4em;
+}
+
+cite {
+  font-size: 1em;
+  color: #666;
+}
+
+.wiki {
+  font-size: 1.5em;
+}
+
+.copyright {
+  border-top: 1px solid #eee;
+  width: 90%;
+  padding-top: 20px;
+  margin: 30px auto;
+  margin-bottom: 10px;
+  padding-bottom: 0;
+  text-align: center;
+  font-size: 0.8em;
+}
+
+.dcmf-logo{height: 25px;}
+</style>
+<body>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="text-center content">
+        <h1>Muhammad Ali</h1>
+        <p><i>Cassius Marcellus Clay Jr.</i></p>
+        <div class="img-container col-lg-10 offset-lg-1">
+          <img src="https://static.comicvine.com/uploads/original/11116/111167398/4281216-0866640711-muham.jpg" alt="Muhammad Ali defeats his enemy in the ring" class="img-css"/>
+          <p>Muhammad Ali defeats his enemy in the ring</p>
+        </div>
+        <div class="col-lg-8 offset-lg-2 biography">
+          <h4>Here is the time line of Muhammad Ali:</h4>
+          <ul>
+            <li><strong>1942</strong> - Born in Louisville, Kentucky</li>
+            <li><strong>1960</strong> - Became an Olympic gold medalist</li>
+            <li><strong>1964</strong> - Became the world heavyweight boxing champion</li>
+            <li><strong>1984</strong> - Diagnosed with Parkinson's disease</li>
+            <li><strong>2005</strong> - Ali devoted much of his time to philanthropy, earning the Presidential Medal of Freedom</li>
+            <li><strong>2016</strong> - died on June 3, 2016, in Phoenix, Arizona</li>
+          </ul>
+          <blockquote>
+            <q>I hated every minute of training, but I said, 'Don't quit. Suffer now and live the rest of your life as a champion.</q>
+            <footer>— <cite>Muhammad Ali</cite></footer>
+          </blockquote>
+          <p class="wiki">If you would like to read more about Ali, check out his page at <a href="https://en.wikipedia.org/wiki/Muhammad_Ali">Wikipedia!</a></p>
+        </div>
+      </div>
+      <div class="col-lg-1"></div>
+    </div>
+  </div>
+  <footer class="copyright">Written and coded by <a href="https://www.dcmf.hu"><img class="dcmf-logo" src="dcmf-letters.png" alt="David's Code ManuFactory logo"/></a></footer>
+</body>
+</html>
